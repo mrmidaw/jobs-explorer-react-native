54727062214517870a1a0aeb686e687c721da386
mrmidaw <mr.midaw@gmail.com>
2023-04-05 17:01:16 +0300

    вакансии поблизости

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 ## Environment Variables
 
-Для локального запуска проекта вам нужно создать в корне проекта .env
-ключ для api вы можете получить
+##### Для локального запуска проекта вам нужно создать в корне проекта .env
+
+##### Ключ для api вы можете получить
 
 - [Rapid API - JSearch](https://rapidapi.com/letscrape-6bRBa3QguO5/api/jsearch)
   `RAPID_API_KEY`
--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 
-import styles from './nearbyjobs.style'
+import { useRouter } from "expo-router";
+
+import { COLORS } from "../../../constants";
+
+import styles from "./nearbyjobs.style";
+import { useFetch } from "../../../hooks/useFetch";
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
 
 const Nearbyjobs = () => {
-  return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Nearbyjobs
+  const { data, isLoading, error, refetch } = useFetch("search", {
+    query: "React Developer",
+    num_pages: 1,
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Вакансии по близости</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Показать все</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading && (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        )}
+
+        {error && <Text>Что-то пошло не так!</Text>}
+
+        {!isLoading &&
+          data?.map((job) => (
+            <NearbyJobCard
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job?.job_id}`)}
+            />
+          ))}
+      </View>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
