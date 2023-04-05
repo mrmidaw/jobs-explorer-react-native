f6612a5de9ddfd4b5db3bab7ececd1db16c55f50
mrmidaw <mr.midaw@gmail.com>
2023-04-05 10:35:20 +0300

    header mvp

--- components/index.js
+++ components/index.js
@@ -1,18 +1,18 @@
 import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn";
 
-// home screen
+// ************HOME SCREENS************
 import Welcome from "./home/welcome/Welcome";
 import Nearbyjobs from "./home/nearby/Nearbyjobs";
 import Popularjobs from "./home/popular/Popularjobs";
 
-// job details screen
+// ************JOB DETAILS SCREENS************
 import Company from "./jobdetails/company/Company";
 import { default as JobTabs } from "./jobdetails/tabs/Tabs";
 import { default as JobAbout } from "./jobdetails/about/About";
 import { default as JobFooter } from "./jobdetails/footer/Footer";
 import Specifics from "./jobdetails/specifics/Specifics";
 
-// common
+// ************COMMON************
 import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
 
 export {
@@ -25,5 +25,5 @@ export {
   JobAbout,
   JobFooter,
   Specifics,
-  NearbyJobCard
+  NearbyJobCard,
 };
